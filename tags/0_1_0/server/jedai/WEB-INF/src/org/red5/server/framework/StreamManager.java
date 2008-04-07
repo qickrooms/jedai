cdbb487f767173e108ace0ab0ad4be87780fdeee
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2008-04-07 18:40:56 +0000

    initial tag

---
+++ tags/0_1_0/server/jedai/WEB-INF/src/org/red5/server/framework/StreamManager.java
@@ -0,0 +1,95 @@
+package org.red5.server.framework;
+
+import org.red5.server.api.IBasicScope;
+import org.red5.server.api.IClient;
+import org.red5.server.api.IConnection;
+import org.red5.server.api.IScope;
+import org.red5.server.api.ScopeUtils;
+import org.red5.server.api.event.IEvent;
+import org.red5.server.api.service.IServiceCall;
+import org.red5.server.api.so.ISharedObject;
+import org.red5.server.api.so.ISharedObjectService;
+import org.red5.server.api.stream.IBroadcastStream;
+import org.red5.server.api.stream.IPlayItem;
+import org.red5.server.api.stream.IPlaylistSubscriberStream;
+import org.red5.server.api.stream.IStreamAwareScopeHandler;
+import org.red5.server.api.stream.ISubscriberStream;
+
+public class StreamManager {
+
+	/** Stores the name of the SharedObject to use. */
+	private String name;
+
+	/** Should the SharedObject be persistent? */
+	private boolean persistent;
+
+	/**
+	 * Create a new instance of the client manager.
+	 * 
+	 * @param name
+	 * 			name of the shared object to use
+	 * @param persistent
+	 * 			should the shared object be persistent
+	 */
+	public StreamManager(String name, boolean persistent) {
+		this.name = name;
+		this.persistent = persistent;
+	}
+
+	/**
+	 * Return the shared object to use for the given scope.
+	 * 
+	 * @param scope
+	 * 			the scope to return the shared object for
+	 * @return the shared object to use
+	 */
+	private ISharedObject getSharedObject(IScope scope) {
+		ISharedObjectService service = (ISharedObjectService) ScopeUtils
+				.getScopeService(scope,
+						ISharedObjectService.class,
+						false);
+		return service.getSharedObject(scope, name, persistent);
+	}
+
+	/**
+	 * A new client connected. This adds the username to
+	 * the shared object of the passed scope. 
+	 * 
+	 * @param scope
+	 * 			scope the client connected to 
+	 * @param username
+	 * 			name of the user that connected
+	 * @param uid
+	 * 			the unique id of the user that connected
+	 */
+	@SuppressWarnings("unchecked")
+	public void addStream(IScope scope, String username, String uid) {
+		ISharedObject so = getSharedObject(scope);
+		so.setAttribute(uid, username);
+	}
+	
+	
+	/**
+	 * A client disconnected. This removes the username from
+	 * the shared object of the passed scope.
+	 * 
+	 * @param scope
+	 * 			scope the client disconnected from
+	 * @param uid
+	 * 			unique id of the user that disconnected
+	 * @return the username of the disconnected user
+	 */
+	@SuppressWarnings("unchecked")
+	public String removeStream(IScope scope, String uid) {
+		ISharedObject so = getSharedObject(scope);
+		if (!so.hasAttribute(uid)) {
+			// SharedObject is empty. This happes when the last client
+			// disconnects.
+			return null;
+		}
+
+		String username = so.getStringAttribute(uid);
+		so.removeAttribute(uid);
+		return username;
+	}
+}
