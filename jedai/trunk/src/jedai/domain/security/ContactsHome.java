8e8f19c3ee2590554db4abfccc4d07d5a1fcd46e
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2009-02-05 20:42:36 +0000

    adding DAO code adding dependent libs adding ProfileVO and ProfileService

---
+++ jedai/trunk/src/jedai/domain/security/ContactsHome.java
@@ -0,0 +1,123 @@
+package jedai.domain.security;
+
+// Generated Feb 5, 2009 3:36:59 PM by Hibernate Tools 3.2.4.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Contacts.
+ * @see jedai.domain.security.Contacts
+ * @author Hibernate Tools
+ */
+public class ContactsHome {
+
+	private static final Log log = LogFactory.getLog(ContactsHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Contacts transientInstance) {
+		log.debug("persisting Contacts instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Contacts instance) {
+		log.debug("attaching dirty Contacts instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Contacts instance) {
+		log.debug("attaching clean Contacts instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Contacts persistentInstance) {
+		log.debug("deleting Contacts instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Contacts merge(Contacts detachedInstance) {
+		log.debug("merging Contacts instance");
+		try {
+			Contacts result = (Contacts) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Contacts findById(long id) {
+		log.debug("getting Contacts instance with id: " + id);
+		try {
+			Contacts instance = (Contacts) sessionFactory.getCurrentSession()
+					.get("jedai.domain.security.Contacts", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Contacts instance) {
+		log.debug("finding Contacts instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria(
+					"jedai.domain.security.Contacts").add(
+					Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
