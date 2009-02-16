5b7003deea370ed4b4896b0ea0082a40d5812069
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2009-02-16 22:11:18 +0000

    now injecting sessionFactory through spring sessionFactory used inside of DAO instead of getting through JNDI

--- jedai/trunk/src/jedai/domain/security/AuthoritiesHome.java
+++ jedai/trunk/src/jedai/domain/security/AuthoritiesHome.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
 import org.hibernate.criterion.Example;
 
 /**
@@ -19,106 +20,165 @@ public class AuthoritiesHome {
 
 	private static final Log log = LogFactory.getLog(AuthoritiesHome.class);
 
-	private final SessionFactory sessionFactory = getSessionFactory();
+	private static SessionFactory sessionFactory;
 
-	protected SessionFactory getSessionFactory() {
-		try {
-			return (SessionFactory) new InitialContext()
-					.lookup("SessionFactory");
-		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
-			throw new IllegalStateException(
-					"Could not locate SessionFactory in JNDI");
-		}
+//	protected SessionFactory getSessionFactory() {
+//		try {
+//			return (SessionFactory) new InitialContext()
+//					.lookup("SessionFactory");
+//		} catch (Exception e) {
+//			log.error("Could not locate SessionFactory in JNDI", e);
+//			throw new IllegalStateException(
+//					"Could not locate SessionFactory in JNDI");
+//		}
+//	}
+	
+	public void setSessionFactory(SessionFactory factory) {
+		System.out.println("factory: " + factory);
+		AuthoritiesHome.sessionFactory = factory;
 	}
 
 	public void persist(Authorities transientInstance) {
 		log.debug("persisting Authorities instance");
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			sessionFactory.getCurrentSession().persist(transientInstance);
+			session.beginTransaction();
+			session.persist(transientInstance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("persist successful");
 		} catch (RuntimeException re) {
 			log.error("persist failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public void attachDirty(Authorities instance) {
 		log.debug("attaching dirty Authorities instance");
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			session.beginTransaction();
+			session.saveOrUpdate(instance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("attach successful");
 		} catch (RuntimeException re) {
 			log.error("attach failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public void attachClean(Authorities instance) {
 		log.debug("attaching clean Authorities instance");
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			session.beginTransaction();
+			session.lock(instance, LockMode.NONE);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("attach successful");
 		} catch (RuntimeException re) {
 			log.error("attach failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public void delete(Authorities persistentInstance) {
 		log.debug("deleting Authorities instance");
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			sessionFactory.getCurrentSession().delete(persistentInstance);
+			session.beginTransaction();
+			session.delete(persistentInstance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("delete successful");
 		} catch (RuntimeException re) {
 			log.error("delete failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public Authorities merge(Authorities detachedInstance) {
 		log.debug("merging Authorities instance");
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			Authorities result = (Authorities) sessionFactory
-					.getCurrentSession().merge(detachedInstance);
+			session.beginTransaction();
+			Authorities result = (Authorities) session.merge(detachedInstance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
 			log.error("merge failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public Authorities findById(jedai.domain.security.AuthoritiesId id) {
 		log.debug("getting Authorities instance with id: " + id);
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			Authorities instance = (Authorities) sessionFactory
-					.getCurrentSession().get(
+			session.beginTransaction();
+			Authorities instance = (Authorities) session.get(
 							"jedai.domain.security.Authorities", id);
 			if (instance == null) {
 				log.debug("get successful, no instance found");
 			} else {
 				log.debug("get successful, instance found");
 			}
+			session.flush();
+			session.getTransaction().commit();
 			return instance;
 		} catch (RuntimeException re) {
 			log.error("get failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public List findByExample(Authorities instance) {
 		log.debug("finding Authorities instance by example");
+		Session session = sessionFactory.getCurrentSession();
 		try {
-			List results = sessionFactory.getCurrentSession().createCriteria(
+			session.beginTransaction();
+			List results = session.createCriteria(
 					"jedai.domain.security.Authorities").add(
 					Example.create(instance)).list();
 			log.debug("find by example successful, result size: "
 					+ results.size());
+			session.flush();
+			session.getTransaction().commit();
 			return results;
 		} catch (RuntimeException re) {
 			log.error("find by example failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 }
