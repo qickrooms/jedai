137ffe3e21d9843d6bbb2d165509ec69250f73fb
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2009-02-16 22:11:18 +0000

    now injecting sessionFactory through spring sessionFactory used inside of DAO instead of getting through JNDI

--- trunk/jedai/trunk/src/jedai/domain/security/UsersHome.java
+++ trunk/jedai/trunk/src/jedai/domain/security/UsersHome.java
@@ -8,6 +8,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.LockMode;
 import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
 import org.hibernate.criterion.Example;
 
 /**
@@ -19,105 +20,173 @@ public class UsersHome {
 
 	private static final Log log = LogFactory.getLog(UsersHome.class);
 
-	private final SessionFactory sessionFactory = getSessionFactory();
+	private static SessionFactory sessionFactory;
 
-	protected SessionFactory getSessionFactory() {
-		try {
-			return (SessionFactory) new InitialContext()
-					.lookup("SessionFactory");
-		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
-			throw new IllegalStateException(
-					"Could not locate SessionFactory in JNDI");
-		}
+//	protected SessionFactory getSessionFactory() {
+//		try {
+//			return (SessionFactory) new InitialContext()
+//					.lookup("SessionFactory");
+//		} catch (Exception e) {
+//			log.error("Could not locate SessionFactory in JNDI", e);
+//			throw new IllegalStateException(
+//					"Could not locate SessionFactory in JNDI");
+//		}
+//	}
+
+	public void setSessionFactory(SessionFactory factory) {
+		System.out.println("factory: " + factory);
+		UsersHome.sessionFactory = factory;
 	}
-
+	
 	public void persist(Users transientInstance) {
 		log.debug("persisting Users instance");
-		try {
-			sessionFactory.getCurrentSession().persist(transientInstance);
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			session.persist(transientInstance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("persist successful");
 		} catch (RuntimeException re) {
 			log.error("persist failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public void attachDirty(Users instance) {
 		log.debug("attaching dirty Users instance");
-		try {
-			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			session.saveOrUpdate(instance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("attach successful");
 		} catch (RuntimeException re) {
 			log.error("attach failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public void attachClean(Users instance) {
 		log.debug("attaching clean Users instance");
-		try {
-			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			session.lock(instance, LockMode.NONE);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("attach successful");
 		} catch (RuntimeException re) {
 			log.error("attach failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public void delete(Users persistentInstance) {
 		log.debug("deleting Users instance");
-		try {
-			sessionFactory.getCurrentSession().delete(persistentInstance);
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			session.delete(persistentInstance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("delete successful");
 		} catch (RuntimeException re) {
 			log.error("delete failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public Users merge(Users detachedInstance) {
 		log.debug("merging Users instance");
-		try {
-			Users result = (Users) sessionFactory.getCurrentSession().merge(
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			Users result = (Users) session.merge(
 					detachedInstance);
+			session.flush();
+			session.getTransaction().commit();
 			log.debug("merge successful");
 			return result;
 		} catch (RuntimeException re) {
 			log.error("merge failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public Users findById(java.lang.String id) {
 		log.debug("getting Users instance with id: " + id);
-		try {
-			Users instance = (Users) sessionFactory.getCurrentSession().get(
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			Users instance = (Users) session.get(
 					"jedai.domain.security.Users", id);
 			if (instance == null) {
 				log.debug("get successful, no instance found");
 			} else {
 				log.debug("get successful, instance found");
 			}
+			session.flush();
+			session.getTransaction().commit();
 			return instance;
 		} catch (RuntimeException re) {
 			log.error("get failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 
 	public List findByExample(Users instance) {
 		log.debug("finding Users instance by example");
-		try {
-			List results = sessionFactory.getCurrentSession().createCriteria(
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {	
+			session.beginTransaction();
+			List results = session.createCriteria(
 					"jedai.domain.security.Users")
 					.add(Example.create(instance)).list();
 			log.debug("find by example successful, result size: "
 					+ results.size());
+			session.flush();
+			session.getTransaction().commit();
 			return results;
 		} catch (RuntimeException re) {
 			log.error("find by example failed", re);
 			throw re;
+		} finally {
+			if (session.isOpen()) {
+				session.close();
+			}
 		}
 	}
 }
