4d7b1badf9c0c5f0e842c323b63d350f8cf84519
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2009-03-04 19:54:52 +0000

    added source headers added registration to authorization service added services for communications layer

--- trunk/jedai/trunk/src/jedai/business/JedaiStreamService.java
+++ trunk/jedai/trunk/src/jedai/business/JedaiStreamService.java
@@ -1,5 +1,24 @@
 package jedai.business;
 
+/*
+ * Jedai Networking Framework - http://jedai.googlecode.com
+ * 
+ * Copyright (c) 2006-2009 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
 import org.red5.server.api.IBasicScope;
 import org.red5.server.api.IClient;
 import org.red5.server.api.IConnection;
@@ -12,6 +31,10 @@ import org.red5.server.api.stream.IPlaylistSubscriberStream;
 import org.red5.server.api.stream.IStreamAwareScopeHandler;
 import org.red5.server.api.stream.ISubscriberStream;
 
+/**
+ * @author dominickaccattato
+ *
+ */
 public class JedaiStreamService implements IStreamAwareScopeHandler {
 
 	@Override
