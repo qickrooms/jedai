a073180ab54e45d8b05326a8dd7e6985e627f531
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2009-03-04 19:54:52 +0000

    added source headers added registration to authorization service added services for communications layer

--- jedai/trunk/src/jedai/domain/security/Users.java
+++ jedai/trunk/src/jedai/domain/security/Users.java
@@ -1,6 +1,23 @@
 package jedai.domain.security;
 
-// Generated Feb 5, 2009 9:48:08 AM by Hibernate Tools 3.2.4.CR1
+/*
+ * Jedai Networking Framework - http://jedai.googlecode.com
+ * 
+ * Copyright (c) 2006-2009 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
