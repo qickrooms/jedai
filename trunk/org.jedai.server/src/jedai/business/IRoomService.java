1d7e9b916c35549de0848024845996311cfbe132
daccattato <daccattato@2911704a-5046-0410-88d6-977042086659>
2009-09-02 01:40:10 +0000

    adding headers, vo's

--- trunk/org.jedai.server/src/jedai/business/IRoomService.java
+++ trunk/org.jedai.server/src/jedai/business/IRoomService.java
@@ -1,5 +1,50 @@
 package jedai.business;
 
+/*
+ * Jedai Networking Framework - http://jedai.googlecode.com
+ * 
+ * Copyright (c) 2006-2009 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+import jedai.domain.Room;
+
+/**
+ * @author dominickaccattato
+ *
+ */
 public interface IRoomService {
+	
+	/**
+	 * @param room
+	 */
+	public void createRoom(Room room);
+	
+	/**
+	 * @param roomId
+	 */
+	public void getRoom(int roomId);
+	
+	/**
+	 * @param room
+	 */
+	public void updateRoom(Room room);
+	
+	/**
+	 * @param room
+	 */
+	public void deleteRoom(Room room);
 
 }
